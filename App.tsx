96bcb31ea05cf2b7cf14ef1de24a9cba087c4bf7
Cody Reeves <cr5697@gmail.com>
2023-03-05 11:56:25 -0800

    Base styling for dashboard

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {
   DefaultTheme
 } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Appearance } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
 import HomeScreen from './screens/HomeScreen';
@@ -13,8 +13,9 @@ import { EventRegister } from 'react-native-event-listeners';
 import themeContext from './config/themeContext';
 import theme from './config/theme';
 import { MyDarkNavTheme } from './interfaces';
-import {} from '@expo-google-fonts/jura';
-
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import {
   useFonts,
   Jura_400Regular,
@@ -31,6 +32,9 @@ import {
   RobotoMono_700Bold_Italic
 } from '@expo-google-fonts/dev';
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
   const [mode, setMode] = useState(false);
 
@@ -41,10 +45,50 @@ export default function App() {
         setMode(data);
       }
     );
+
     return () => {
       EventRegister.removeEventListener('eventListener');
     };
   });
+
+  //================== Zombie code which is supposed to handle displaying
+  //a loading icon until the fonts load. Currently getting the following error
+  // 'Cannot load null or undefined font source: "http://fonts.gstatic.com/s/robotomono/v13/blahblahblah": undefined }. '
+  // const [appIsReady, setAppIsReady] = useState(false);
+
+  // useEffect(() => {
+  //   async function prepare() {
+  //     try {
+  //       // Pre-load fonts, make any API calls you need to do here
+  //       //await Font.loadAsync(Entypo.font);
+  //       await Font.loadAsync(RobotoMono_400Regular);
+  //     } catch (e) {
+  //       console.warn(e);
+  //     } finally {
+  //       // Tell the application to render
+  //       setAppIsReady(true);
+  //     }
+  //   }
+  //   prepare();
+  // }, []);
+
+  // const onLayoutRootView = useCallback(
+  //   async () => {
+  //     if (appIsReady) {
+  //       // This tells the splash screen to hide immediately! If we call this after
+  //       // `setAppIsReady`, then we may see a blank screen while the app is
+  //       // loading its initial state and rendering its first pixels. So instead,
+  //       // we hide the splash screen once we know the root view has already
+  //       // performed layout.
+  //       await SplashScreen.hideAsync();
+  //     }
+  //   },
+  //   [appIsReady]
+  // );
+
+  // if (!appIsReady) {
+  //   return null;
+  // }
 
   return (
     <themeContext.Provider
@@ -58,5 +102,3 @@ export default function App() {
     </themeContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({});
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View, Button, Switch } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Switch,
+  Pressable
+} from 'react-native';
 import React, { useState, useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { EventRegister } from 'react-native-event-listeners';
 import themeContext from './../config/themeContext';
 import theme from '../config/theme';
 import UITheme from '../interfaces';
+import {
+  RobotoMono_400Regular,
+  useFonts
+} from '@expo-google-fonts/dev';
+import AppLoading from 'expo-app-loading';
 
 type Props = {
   navigation: any;
@@ -13,6 +25,10 @@ type Props = {
 const HomeScreen = ({ navigation, ...props }: Props) => {
   const theme: any = useContext(themeContext);
   const [mode, setMode] = useState(false);
+  // let [fontsLoaded] = useFonts({
+  //   RobotoMono_400Regular
+  // });
+
   return (
     <View
       style={[
@@ -20,7 +36,7 @@ const HomeScreen = ({ navigation, ...props }: Props) => {
         { backgroundColor: theme.background }
       ]}
     >
-      <Text style={[styles.text, { color: theme.color }]}>
+      <Text style={[styles.textTitle, { color: theme.color }]}>
         Welcome to your Dashboard. Let's drum!
       </Text>
       <Switch
@@ -30,10 +46,41 @@ const HomeScreen = ({ navigation, ...props }: Props) => {
           EventRegister.emit('changeTheme', value);
         }}
       />
+      <View style={styles.cardContainer}>
+        <View style={[styles.RowOne]}>
+          <View style={[styles.cardRowOne, styles.cardAchievements]}>
+            <Text
+              style={[styles.textCard, { color: theme.color }]}
+              onPress={() => navigation.navigate('Achievements')}
+            >
+              Achievements
+            </Text>
+          </View>
+          <View style={[styles.cardRowOne, styles.cardStreakTracker]}>
+            <Text style={[styles.textCard, { color: theme.color }]}>
+              Current Streak
+            </Text>
+          </View>
+        </View>
+        <View style={[styles.RowTwo]}>
+          <View
+            style={[styles.cardRowTwo, styles.cardActivityTracker]}
+          >
+            <Text style={[styles.textCard, { color: theme.color }]}>
+              Recent Activity
+            </Text>
+          </View>
+        </View>
+      </View>
       <View style={styles.button}>
-        <Button
-          title="View Achievements"
+        <Pressable
           onPress={() => navigation.navigate('Achievements')}
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? '#5DAAF1' : '#7DB7ED'
+            },
+            styles.startRoutineButton
+          ]}
         />
       </View>
     </View>
@@ -45,15 +92,74 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center'
   },
-  text: {
+  textCard: {
+    fontSize: 12,
+    fontWeight: 'normal'
+  },
+  textTitle: {
     fontWeight: 'bold',
     fontSize: 20,
-    paddingBottom: 20
+    paddingBottom: 20,
+    margin: 20
   },
   button: {
     paddingTop: 20
-  }
+  },
+  startRoutineButton: {
+    borderRadius: 50,
+    width: '95px',
+    height: '95px'
+  },
+  startRoutineButtonPressed: {
+    borderRadius: 50,
+    width: '95px',
+    height: '95px'
+  },
+  cardContainer: {
+    display: 'flex',
+    height: '40%',
+    width: '90%',
+    marginTop: 30,
+    barginBottom: 30
+  },
+  cardRowOne: {
+    border: '1px solid #CECECE',
+    borderRadius: 3,
+    width: '45%',
+    height: '90%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  RowOne: {
+    width: '100%',
+    height: '50%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  cardAchievements: {},
+  cardStreakTracker: {},
+  RowTwo: {
+    width: '100%',
+    height: '50%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cardRowTwo: {
+    border: '1px solid #CECECE',
+    borderRadius: 3,
+    width: '95%',
+    height: '90%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cardActivityTracker: {}
 });
